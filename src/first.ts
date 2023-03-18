6565af0149cb2bc1f49e9ae817fbb7179e5b0a54
sunblock99 <sunblock99@kakao.com>
2023-03-18 16:44:55 +0900

    function에 리턴타입만들어주기

--- src/first.ts
+++ src/first.ts
@@ -7,21 +7,27 @@ type Player = {
     age? : Age
 }
 
+// 방법1
+// //                              : 리턴타입
+// function playerMaker(name:Name) : Player{
+//     return {
+//         // return 값이름과 인자값 이름이 같으면 :인자명 생략가능 
+//         name:name,
+//     }
+// } 
+
+// 방법2
+const playerMaker = (name:Name) : Player => ({name});
 
 
-const sun : Player = {
-    name : "선형채"
-}
 
-const zzong : Player = {
-    name:"쫑이",
-    age:11
-}
+const sun = playerMaker("선형채");
+sun.age = 24
 
 
 
 
-// typescript에서 age? 로 선언후 player.age를 해줌으로써 에러가 안나게 해줌
-if(sun.age && sun.age < 10){
+// // typescript에서 age? 로 선언후 player.age를 해줌으로써 에러가 안나게 해줌
+// if(sun.age && sun.age < 10){
 
-}
+// }
